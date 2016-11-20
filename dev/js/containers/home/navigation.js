2a3f60168b7866724fb63ea33cf2f053dd7c56d8
knightzf <knightzf@gmail.com>
2016-11-19 19:15:34 -0500

    nav left

--- dev/js/containers/home/navigation.js
+++ dev/js/containers/home/navigation.js
@@ -1,39 +1,118 @@
 import React, {Component} from 'react';
 import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
-import {Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColumn} from 'material-ui/Table';
+import Divider from 'material-ui/Divider';
+import Avatar from 'material-ui/Avatar';
 require('../../../scss/style.scss');
 
 class Navigation extends Component {
   render() {
-    var style = {padding: 0};
+    var icon_style = { fontSize: "24px",
+      color: "rgba(0, 0, 0, 0.54)"
+    };
+
+    var topData = [
+      {
+        id: 1,
+        iconName: "home",
+        label: "Home"
+      },
+      {
+        id: 2,
+        iconName: "account_circle",
+        label: "My Blogs"
+      },
+      {
+        id: 3,
+        iconName: "list",
+        label: "Subscriptions"
+      }
+    ];
+
+    var topList = topData.map((row) => {
+      return (
+        <tr className="nav_row_cls" key={row.id}>
+          <td className="icon_cell_cls">
+            <i className="material-icons" style={icon_style}>{row.iconName}</i>
+          </td>
+          <td className="label_cell_cls">
+            {row.label}
+          </td>
+          <td className="count_cell_cls">
+          </td>
+        </tr>
+      );
+    });
+
+    var subscriptionData = [
+      {
+        id: 1,
+        avatar: "panda.png",
+        blogName: "Love Live",
+        newPosts: 2
+      }
+    ];
+
+    var subscriptionList = subscriptionData.map((row) => {
+      return (
+        <tr className="nav_row_cls" key={row.id}>
+          <td className="icon_cell_cls">
+            <Avatar size={24} src={row.avatar} />
+          </td>
+          <td className="label_cell_cls">
+            {row.blogName}
+          </td>
+          <td className="count_cell_cls">
+            {row.newPosts}
+          </td>
+        </tr>
+      );
+    });
+
     return (
       <div className="left_side_cls">
-      <Table style={style}>
-        <TableBody
-          displayRowCheckbox={false}
-        >
-          <TableRow
-            displayBorder={false}
-          >
-            <TableRowColumn style={style}>1</TableRowColumn>
-            <TableRowColumn style={style}>Home</TableRowColumn>
-          </TableRow>
-          <TableRow
-            displayBorder={false}
-          >
-            <TableRowColumn style={style}>2</TableRowColumn>
-            <TableRowColumn style={style}>My Blogs</TableRowColumn>
-          </TableRow>
-          <TableRow
-            displayBorder={false}
-          >
-            <TableRowColumn style={style}>3</TableRowColumn>
-            <TableRowColumn style={style}>Subscriptions</TableRowColumn>
-          </TableRow>
-        </TableBody>
-      </Table>
+        <div className="div_cls">
+          <Divider />
+        </div>
 
+        <table className="nav_table_cls">
+          <tbody>
+            {topList}
+          </tbody>
+        </table>
+
+        <div className="div_cls">
+          <Divider />
+        </div>
+
+        <div className="lib_div_cls">LIBRARY</div>
+        <div className="div_cls" />
+        <table className="nav_table_cls">
+          <tbody>
+            <tr className="nav_row_cls">
+              <td className="icon_cell_cls">
+                <i className="material-icons" style={icon_style}>thumb_up</i>
+              </td>
+              <td className="label_cell_cls">
+                Liked Posts
+              </td>
+              <td className="count_cell_cls">
+              </td>
+            </tr>
+          </tbody>
+        </table>
+
+        <div className="div_cls">
+          <Divider />
+        </div>
+
+        <div className="lib_div_cls">SUBSCRIPTIONS</div>
+        <div className="div_cls" />
+        <table className="nav_table_cls">
+          <tbody>
+            {subscriptionList}
+          </tbody>
+        </table>
       </div>
     );
   }
